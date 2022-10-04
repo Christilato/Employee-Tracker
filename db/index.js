0b3f3424eb69789c92a7f040f98a8bccb0964ad9
Christi Lato <christilato@Christis-MacBook-Air.local>
2022-10-04 09:46:32 -0500

    findAllEmployees not working

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ class DB {
     }
     findAllEmployees () {
         return this.connection.promise().query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.name_role, departments.name_department AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id LEFT JOIN departments on role.department_id = departments.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+            "SELECT employee.id, employee.first_name, employee.last_name, role.name_role, departments.name_department AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN departments ON role.department_id = departments.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
         );
     
     }
